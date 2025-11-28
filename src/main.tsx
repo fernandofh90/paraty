329238869062ee13dc63af3c2946a26080cb7f28
Fernando Silva <76487937+fernandofh90@users.noreply.github.com>
2025-11-27 21:54:48 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import 'leaflet/dist/leaflet.css'; // Importação crítica para o mapa funcionar corretamente
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
