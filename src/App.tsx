216e27d766841b088798ca5a1a2ff07abafb5588
Fernando Silva <fernandofh90@hotmail.com>
2025-11-27 21:27:54 -0300

    tsconig
    Changes tsconfig and app

--- src/App.tsx
+++ src/App.tsx
@@ -11,39 +11,39 @@ import { PlacesProvider } from './contexts/PlacesContext';
 
 // A layout wrapper to handle distinct areas
 const AppLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  return (
-    <div className="w-full h-screen bg-gray-50 flex flex-col overflow-hidden">
-      {children}
-    </div>
-  );
+    return (
+        <div className="w-full h-screen bg-gray-50 flex flex-col overflow-hidden">
+            {children}
+        </div>
+    );
 };
 
 const App: React.FC = () => {
-  return (
-    <PlacesProvider>
-      <LanguageProvider>
-        <HashRouter>
-          <AppLayout>
-            <Routes>
-              {/* Visitor Routes */}
-              <Route path="/" element={<Welcome />} />
-              <Route path="/map" element={<VisitorMap />} />
-              
-              {/* Advertiser Routes */}
-              <Route path="/advertiser/login" element={<AdvertiserLogin />} />
-              <Route path="/advertiser/register" element={<AdvertiserRegister />} />
-              <Route path="/advertiser/dashboard" element={<AdvertiserDashboard />} />
-              
-              {/* Admin Routes */}
-              <Route path="/admin" element={<AdminDashboard />} />
+    return (
+        <PlacesProvider>
+            <LanguageProvider>
+                <HashRouter>
+                    <AppLayout>
+                        <Routes>
+                            {/* Visitor Routes */}
+                            <Route path="/" element={<Welcome />} />
+                            <Route path="/map" element={<VisitorMap />} />
 
-              <Route path="*" element={<Navigate to="/" replace />} />
-            </Routes>
-          </AppLayout>
-        </HashRouter>
-      </LanguageProvider>
-    </PlacesProvider>
-  );
+                            {/* Advertiser Routes */}
+                            <Route path="/advertiser/login" element={<AdvertiserLogin />} />
+                            <Route path="/advertiser/register" element={<AdvertiserRegister />} />
+                            <Route path="/advertiser/dashboard" element={<AdvertiserDashboard />} />
+
+                            {/* Admin Routes */}
+                            <Route path="/admin" element={<AdminDashboard />} />
+
+                            <Route path="*" element={<Navigate to="/" replace />} />
+                        </Routes>
+                    </AppLayout>
+                </HashRouter>
+            </LanguageProvider>
+        </PlacesProvider>
+    );
 };
 
 export default App;
